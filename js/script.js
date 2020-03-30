9cfcb3c37bde359e7ef0d0b8f9b5b7fa0e48398d
dejvoss <deosiecki@gmail.com>
2020-03-30 18:51:03 +0200

    Add drawing table line script

--- js/script.js
+++ js/script.js
@@ -1,17 +1,23 @@
+// function for writing category names
 var categoryNames = "Country City Animal Thing Plant Name Car Points";
 var letterCtr = 0;
 var headerIndx = 0;
 var speed = 120;
 
 function wrtieCategory() {
-    var hdrTag = document.getElementById("headerRow").getElementsByTagName("th");
-  if (letterCtr < categoryNames.length) { 
-      if (categoryNames.charAt(letterCtr) == " ") {
-        headerIndx++;
-      }
-    hdrTag[headerIndx].innerHTML += categoryNames.charAt(letterCtr);
-    letterCtr++;
-    console.log(letterCtr);
-    setTimeout(wrtieCategory, speed);
-  }
+    var hdrTag = document.getElementById("headerRow").getElementsByTagName("th"); // take table header
+    if (letterCtr < categoryNames.length) {
+        if (categoryNames.charAt(letterCtr) == " ") { //check if text in category names has space, if yes change the header index
+            headerIndx++;
+        }
+        hdrTag[headerIndx].innerHTML += categoryNames.charAt(letterCtr);
+        letterCtr++;
+        setTimeout(wrtieCategory, speed);
+    }
 }
+
+function drawTable() {
+    document.getElementById("GameTable").classList.add("bottomLine");
+ 
+}
+
