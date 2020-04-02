1351d9885db812d56b3638d3747b6447f240c793
dejvoss <deosiecki@gmail.com>
2020-04-03 00:07:54 +0200

    Adjust sections for full width

--- js/game_script.js
+++ js/game_script.js
@@ -5,7 +5,9 @@ function openUserNameForm() {
 }
 // Close user name form by clicking cancel
 function closeUserNameForm() {
+    document.getElementById("userNameInp").value = "";
     document.getElementById("userNameForm").style.display = "none";
+    
   }
 
 //   Click next in user name form
@@ -14,13 +16,16 @@ var userName;
 function openLeterForm() {
     var inputForm = document.getElementById("userNameInp");
     if (inputForm.value == "") {
-        document.getElementById("noMessSpan").style.display = "block";
+        document.getElementById("noMessSpan").style.display = "block"; // show message when userName field is empty
     } else {
         userName = inputForm.value;
+        document.getElementById("userNameForm").style.display = "none";
+        document.getElementById("gameModeForm").style.display = "block";
     }      
     console.log(userName);
 };
 
+// remove message when user name was empty but user click on user name input field
 $("#userNameInp").focus(function(){
     document.getElementById("noMessSpan").style.display = "none";
 });
